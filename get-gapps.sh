3689e1b059a942673ca5c63170cd836f0d39ea3f
Johannes Baiter <johannes.baiter@gmail.com>
2012-04-22 21:29:28 +0200

    Fixed path in get-gapps.sh

--- get-gapps.sh
+++ get-gapps.sh
@@ -6,10 +6,10 @@ GAPPS_TMP=/tmp/gapps-ics
 
 wget -O/tmp/gapps-ics.zip "$GAPPS_URL"
 unzip $GAPPS_ZIP -d $GAPPS_TMP
-mkdir -p gapps/system
-mv $GAPPS_TMP/system/app $GAPPS_TMP/system/etc $GAPPS_TMP/system/framework $GAPPS_TMP/system/lib $GAPPS_TMP/system/tts gapps/system/
-mv $GAPPS_TMP/optional/face/app/FaceLock.apk gapps/system/app
-mv $GAPPS_TMP/optional/face/lib/libfacelock_jni.so gapps/system/lib/libfacelock_jni.so
-mv $GAPPS_TMP/optional/face/vendor gapps/system/
+mkdir gapps
+mv $GAPPS_TMP/app $GAPPS_TMP/etc $GAPPS_TMP/system/framework $GAPPS_TMP/lib $GAPPS_TMP/tts gapps/
+mv $GAPPS_TMP/optional/face/app/FaceLock.apk gapps/app
+mv $GAPPS_TMP/optional/face/lib/libfacelock_jni.so gapps/lib/libfacelock_jni.so
+mv $GAPPS_TMP/optional/face/vendor gapps/
 
 rm -rf $GAPPS_TMP $GAPPS_ZIP
